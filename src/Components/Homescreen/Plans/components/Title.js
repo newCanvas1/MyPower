b5c239509eb5187b3cd6c78058db12b3da427d0f
newCanvas1 <123483334+newCanvas1@users.noreply.github.com>
2024-06-06 19:40:40 +0300

    - Edit.

--- src/Components/Homescreen/Plans/components/Title.js
+++ src/Components/Homescreen/Plans/components/Title.js
@@ -5,6 +5,7 @@ import { styles } from "../../../../styles/styles";
 import { LanguageContext } from "../../../../../context/LanguageContext";
 import { langChoice } from "../../../../utility/functions/langChoice";
 import { ARABIC, ENGLISH } from "../../../../utility/labels";
+import Icon from "react-native-vector-icons/MaterialIcons";
 
 function Title(props) {
   const { language } = useContext(LanguageContext);
@@ -13,10 +14,30 @@ function Title(props) {
     router.push("/createPlan");
   }
   return (
-    <View className={`${langChoice(language," flex-row","flex-row-reverse")} items-center justify-between `}>
-      <Text style={{ fontFamily: langChoice(language,"en","ar") }} className="text-2xl font-bold ">
-        {langChoice(language, ENGLISH.PLANS, ARABIC.PLANS)}
-      </Text>
+    <View
+      className={`${langChoice(
+        language,
+        " flex-row",
+        "flex-row-reverse"
+      )} items-center justify-between `}
+    >
+      <View
+        className={`${langChoice(
+          language,
+          "flex-row-reverse",
+          "flex-row"
+        )} items-center justify-between gap-2`}
+      >
+        <Text
+          style={{ fontFamily: langChoice(language, "en", "ar") }}
+          className="text-2xl font-bold "
+        >
+          {langChoice(language, ENGLISH.PLANS, ARABIC.PLANS)}
+        </Text>
+        <View className="bg-green-500 rounded p-1 shadow">
+          <Icon name="newspaper" size={20} color="white" />
+        </View>
+      </View>
       <TouchableOpacity className={styles.addBtn} onPress={addPlan}>
         <Text className="text-xl text-white">+</Text>
       </TouchableOpacity>
