b6901a1251a1b4985bc6ecc90a1ae5d8eea136e4
newCanvas1 <123483334+newCanvas1@users.noreply.github.com>
2024-06-21 00:08:27 +0300

    - Added carousel index indicator.

--- src/Components/Homescreen/CarouselElement/CarouselElement.js
+++ src/Components/Homescreen/CarouselElement/CarouselElement.js
@@ -6,20 +6,31 @@ import WorkoutsCalender from "../WorkoutsCalender/WorkoutsCalender";
 function CarouselElement(props) {
   const width = Dimensions.get("window").width;
   const list = [<WorkoutsCalender />, <Text>test</Text>];
+  const [currentIndex, setCurrentIndex] = React.useState(0);
   return (
     <View className={"mt-20  h-[50%]"}>
       <Carousel
         loop
         width={width}
         data={list}
-        scrollAnimationDuration={1000}
-        onSnapToItem={(index) => console.log("current index:", index)}
+        scrollAnimationDuration={500}
+        onSnapToItem={(index) => setCurrentIndex(index)}
         renderItem={({ index }) => (
           <View key={index} className="h-full">
             {list[index]}
           </View>
         )}
       />
+      <View className="flex flex-row justify-center">
+        {list.map((item, index) => (
+          <View
+            key={index}
+            className={` ${
+              currentIndex == index ? "w-3 bg-slate-500" : "w-2 bg-sky-300"
+            } h-2  self-center rounded  mx-1`}
+          ></View>
+        ))}
+      </View>
     </View>
   );
 }
